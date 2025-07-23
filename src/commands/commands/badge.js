3d8be0aae2886a381c068a375c24c619294a9577
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-23 00:01:38 -0400

    first commit

---
+++ src/commands/commands/badge.js
@@ -0,0 +1,25 @@
+const { EmbedBuilder } = require("discord.js");
+const fs = require("fs/promises");
+
+module.exports = {
+    data: {
+        name: "badge",
+        description: "Get a random badge from Grace",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+      const badgesJsonData = await fs.readFile("data/badges.json", {encoding: "utf8"});
+      const badgesMap = JSON.parse(badgesJsonData);
+      const badges = badgesMap["badges"];
+      const badge = badges[Math.floor(Math.random() * badges.length)];
+
+      const embed = new EmbedBuilder()
+      .setTitle(`${badge.name}`)
+      .setDescription(`${badge.description}`)
+      .setThumbnail(`https://cdn.sylvee.xyz/${badge.path}.png`)
+
+      await interaction.reply({ embeds: [embed] });
+    },
+};
